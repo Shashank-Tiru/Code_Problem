470196380056df75af95036ca701d6f426238da8
shashank7t <shashank7ishant@gmail.com>
2018-08-14 09:03:37 -0500

    Problem_2

--- test_1.py
+++ test_1.py
@@ -1,7 +1,9 @@
-#Problem_2
+# Problem_2
 
 n = int(input("No. of students: "))
 c = int(input("No. of chairs: "))
-math_students = n/3
-phy_students = n/3
-chem_students = n/3
+math_students = n / 3
+phy_students = n / 3
+chem_students = n / 3
+
+# Can't go beyond this, cuz am not good with the logic for this problem.
